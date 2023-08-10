cb21342b1a00b240aed5fc58c005a03fae22c311
Alex Honeyfield <alex.honeyfield@gmail.com>
2023-08-10 17:55:35 +1200

    cleaned up code

--- PHF2BIN/PHF2BIN.cs
+++ PHF2BIN/PHF2BIN.cs
@@ -1,110 +1,133 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Plugins;
 using System.IO;
 
 namespace PHF2BIN
 {
     public partial class PHF2BIN : Form
     {
-        FileFormatPluginLoader pluginLoader;
-
-
-
         public PHF2BIN()
         {
             InitializeComponent();
-            pluginLoader = new FileFormatPluginLoader(Directory.GetCurrentDirectory());
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFile();
-
         }
 
         void OpenFile()
         {
             var openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = pluginLoader.GetAllFormatsFilter();
+            openFileDialog.Filter = "PHF File|*.phf";
             DialogResult result = openFileDialog.ShowDialog();
             if (result != DialogResult.OK) return;
-            var fileName = openFileDialog.FileName;
+            string fileName = openFileDialog.FileName;
 
-            string extension = Path.GetExtension(fileName).ToLowerInvariant().Replace(".", "");
             if (!File.Exists(fileName))
             {
                 MessageBox.Show("File does not exist: " + fileName, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
-            byte[] rawBinaryFile;
+            byte[] rawBinaryFile = ParseFile(File.ReadAllBytes(fileName));
 
-            Plugins.FileFormat fileFormatPlugin;
-
-            if (pluginLoader.TryGetFileFormat(extension, out fileFormatPlugin))
+            if (rawBinaryFile != null)
             {
-                if (fileFormatPlugin.Open(fileName))
+                SaveFile(rawBinaryFile);
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Invalid file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        int FindBytes(byte[] rawBinary, byte[] needle, int searchLimit = int.MaxValue)
+        {
+            int len = needle.Length;
+            int limit = rawBinary.Length - len < searchLimit ? rawBinary.Length - len : searchLimit;
+            for (int i = 0; i <= limit; i++)
+            {
+                int k = 0;
+                for (; k < len; k++)
                 {
-                    if (fileFormatPlugin.TryReadBytes(out rawBinaryFile))
-                    {
-                        if (rawBinaryFile.Length > 0)
-                        {
-                            SaveFile(rawBinaryFile);
-                            return;
-                        }
-                    }
+                    byte searchByte = needle[k];
+                    byte binaryByte = rawBinary[i + k];
+                    if (searchByte != binaryByte) break;
+                }
+                if (k == len) return i;
+            }
+            return -1;
+        }
+
+        byte[] ParseFile(byte[] bytes)
+        {
+            byte[] rawBinary;
+
+            int outputSize;
+            byte[] header = new byte[0x12];
+            byte[] spanishOak = ASCIIEncoding.ASCII.GetBytes("SPANISHOAK");
+            byte[] blackOak = ASCIIEncoding.ASCII.GetBytes("BOAK");
+            if (FindBytes(bytes, spanishOak, 0x100) != -1)
+            {
+                header[0x10] = 0x10;
+                header[0x11] = 0x60;
+                outputSize = 0x100000;
+            }
+            else if (FindBytes(bytes, blackOak, 0x100) != -1)
+            {
+                header[0x10] = 0x30;
+                header[0x11] = 0x60;
+                outputSize = 0x170000;
+            }
+            else return null;
+
+            rawBinary = new byte[outputSize];
+
+            //Find the magic header bytes
+            int offset = FindBytes(bytes, header);
+            if (offset == -1) return null;
+
+            int binIndex = 0;
+            for (int phfIndex = offset; phfIndex < bytes.Length;)
+            {
+                //8 Byte header before we start again
+                if (binIndex % 0x10000 == 0 && binIndex != 0) phfIndex += 8;
+                if (binIndex >= rawBinary.Length) break;
+
+                //32768-65536 is missing??
+                if (binIndex >= 0x8000 && binIndex < 0x10000)
+                {
+                    rawBinary[binIndex] = 0xFF;
+                    binIndex++;
+                }
+                else
+                {
+                    //Every 32 byte block we have a 6 byte header to remove
+                    if (binIndex % 32 == 0 && binIndex != 0) phfIndex += 6;
+
+                    rawBinary[binIndex] = bytes[phfIndex];
+                    phfIndex++;
+                    binIndex++;
                 }
             }
-            MessageBox.Show("Invalid file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            return;
+
+            return rawBinary;
         }
 
         void SaveFile(byte [] rawBinary)
         {
             SaveFileDialog savefile = new SaveFileDialog();
 
-            //savefile.Filter = pluginLoader.GetIndividualFormatsFilter();
             savefile.Filter = "Binary File|*.bin";
 
             if (savefile.ShowDialog() == DialogResult.OK)
             {
-
-                string extension = Path.GetExtension(savefile.FileName).ToLowerInvariant().Replace(".", "");
-
-                Plugins.FileFormat fileFormatPlugin;
-                if (pluginLoader.TryGetFileFormat(extension, out fileFormatPlugin))
-                {
-                    try
-                    {
-                        fileFormatPlugin.WriteBytes(rawBinary, savefile.FileName);
-
-                        MessageBox.Show("Successfully Saved File!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message, "Error Saving File", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("File format not implemented", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-
-
+                File.WriteAllBytes(savefile.FileName, rawBinary);
             }
         }
-
-
-
-
     }
 }
